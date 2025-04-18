9e2efde3af85c5da92d293edde53dea37f5980e1
fabiovmat <fabio.meihua08@gmail.com>
2025-04-18 17:25:41 -0300

    third commit

---
+++ insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.atletas(
+	id, nome, idade, tempo_treino, categoria1, nota1, classificacao1, categoria2, nota2, classificacao2, categoria3, nota3, classificacao3, categoria4, nota4, classificacao4, categoria5, nota5, classificacao5)
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
